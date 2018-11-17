502f9ba30a6ab004df3efa10d4c63838b4eab768
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-11-16 20:42:49 -0700

    Add working file upload code.

--- animal-bot/scripts/upload-images.js
+++ animal-bot/scripts/upload-images.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const request = require('request');
 const {Storage} = require('@google-cloud/storage');
-// Creates a client
-const storage = new Storage({
-  projectId: 'serverless-animal-bot',
-});
-const myBucket = storage.bucket('serverless-animal-bot-vcs');
 
-const file = myBucket.file('my-file.jpg');
+const storage = new Storage({projectId: 'serverless-animal-bot'});
+const myBucket = storage.bucket('serverless-animal-bot-vcm');
+const images = require('./images.json');
 
-// request('https://vignette.wikia.nocookie.net/space-dandy/images/5/53/Dandy_portal.jpeg')
-//   .pipe(fs.createWriteStream('my-file.jpg'))
-fs.createReadStream('my-file.jpg')
-  .pipe(file.createWriteStream())
-  .on('error', function(err) {
-    console.log(err);
-  })
-  .on('finish', function() {
-    // The file upload is complete.
-    console.log('Complete!');
-  });
-
-
+(async () => {
+  const uploads = Object.keys(images).reduce((promises, key) => {
+    const arr = images[key].map((image, index) => {
+      return new Promise((resolve, reject) => {
+        let fileName = image.url.replace(/.*?\/\/.*?(?<!\/)\/([^\/]+\.(jpeg|jpg|gif|png)).*$/gi, '$1');
+        if (image.url.length === fileName.length) fileName = `${key}${Math.random()}.jpg`;
+        const file = myBucket.file(`/${key}/${index}-${fileName}`);
+        request(image.url)
+          .pipe(file.createWriteStream())
+          .on('error', reject)
+          .on('finish', () => {
+            console.log(fileName);
+            resolve();
+          });
+      });
+    });
+    return promises.concat(arr);
+  }, []);
+  await Promise.all(uploads);
+})();
