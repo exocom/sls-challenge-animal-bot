1666b6f2adca6ad144d9d7afd77948d8ddf25406
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-11-16 21:11:19 -0700

    Add csv file list code.

--- animal-bot/scripts/upload-images.js
+++ animal-bot/scripts/upload-images.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const request = require('request');
+const {Readable} = require('stream');
 const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({projectId: 'serverless-animal-bot'});
-const myBucket = storage.bucket('serverless-animal-bot-vcm');
+const bucket = 'serverless-animal-bot-vcm';
+const myBucket = storage.bucket(bucket);
 const images = require('./images.json');
 
 (async () => {
@@ -12,17 +14,28 @@ const images = require('./images.json');
       return new Promise((resolve, reject) => {
         let fileName = image.url.replace(/.*?\/\/.*?(?<!\/)\/([^\/]+\.(jpeg|jpg|gif|png)).*$/gi, '$1');
         if (image.url.length === fileName.length) fileName = `${key}${Math.random()}.jpg`;
-        const file = myBucket.file(`/${key}/${index}-${fileName}`);
+        const filePath = `/${key}/${index}-${fileName}`;
+        const file = myBucket.file(filePath);
+
         request(image.url)
           .pipe(file.createWriteStream())
           .on('error', reject)
           .on('finish', () => {
             console.log(fileName);
-            resolve();
+            resolve({
+              path: `gs://${bucket}${filePath}`,
+              tag: key
+            });
           });
       });
     });
     return promises.concat(arr);
   }, []);
-  await Promise.all(uploads);
+
+  const files = await Promise.all(uploads);
+  const filesCsv = files.map(({path, tag}) => `${path},${tag}`).join(`\n`);
+  const csv = new Readable();
+  csv.push(filesCsv);
+  csv.push(null);
+  csv.pipe(myBucket.file('files.csv').createWriteStream());
 })();
