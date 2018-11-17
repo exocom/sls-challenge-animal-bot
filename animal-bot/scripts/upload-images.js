354193586314ea1a834918398ac915d43d09d359
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-11-16 22:10:09 -0700

    fix massive upload lag

--- animal-bot/scripts/upload-images.js
+++ animal-bot/scripts/upload-images.js
@@ -10,24 +10,31 @@ const myBucket = storage.bucket(bucket);
 const images = require('./images.json');
 
 (async () => {
-  const uploads = Object.keys(images).reduce((promises, key) => {
+  const uploads = Object.keys(images).reduce((promises, key, i) => {
     const arr = images[key].filter(i => !/img\.gawkerassets\.com/gi.test(i.url)).map((image, index) => {
       return new Promise((resolve, reject) => {
         let fileName = image.url.replace(/.*?\/\/.*?(?<!\/)\/([^\/]+\.(jpeg|jpg|gif|png)).*$/gi, '$1').replace(',', '');
         if (image.url.length === fileName.length) fileName = `${key}${Math.random()}.jpg`;
         const filePath = `/${key}/${index}-${fileName}`;
         const file = myBucket.file(filePath);
-
-        request(image.url)
-          .pipe(file.createWriteStream())
-          .on('error', reject)
-          .on('finish', () => {
-            console.log(fileName);
-            resolve({
-              path: `gs://${bucket}${filePath}`,
-              tag: key
+        let delay = i * 15000;
+        if (index > 50) delay += 500;
+        if (index > 100) delay += 1500;
+        if (index > 150) delay += 2500;
+        if (index > 200) delay += 3500;
+        if (index > 250) delay += 4500;
+        setTimeout(() => {
+          request(image.url)
+            .pipe(file.createWriteStream())
+            .on('error', reject)
+            .on('finish', () => {
+              console.log(fileName);
+              resolve({
+                path: `gs://${bucket}${filePath}`,
+                tag: key
+              });
             });
-          });
+        }, delay);
       });
     });
     return promises.concat(arr);
