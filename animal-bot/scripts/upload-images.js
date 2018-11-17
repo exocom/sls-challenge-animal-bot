1df846359e4f5076609979741520ed276a1d7ab0
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-11-16 21:56:35 -0700

    filter a site. using q to ignore failed uploads

--- animal-bot/scripts/upload-images.js
+++ animal-bot/scripts/upload-images.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const request = require('request');
+const Q = require('q');
 const {Readable} = require('stream');
 const {Storage} = require('@google-cloud/storage');
 
@@ -10,9 +11,9 @@ const images = require('./images.json');
 
 (async () => {
   const uploads = Object.keys(images).reduce((promises, key) => {
-    const arr = images[key].map((image, index) => {
+    const arr = images[key].filter(i => !/img\.gawkerassets\.com/gi.test(i.url)).map((image, index) => {
       return new Promise((resolve, reject) => {
-        let fileName = image.url.replace(/.*?\/\/.*?(?<!\/)\/([^\/]+\.(jpeg|jpg|gif|png)).*$/gi, '$1');
+        let fileName = image.url.replace(/.*?\/\/.*?(?<!\/)\/([^\/]+\.(jpeg|jpg|gif|png)).*$/gi, '$1').replace(',', '');
         if (image.url.length === fileName.length) fileName = `${key}${Math.random()}.jpg`;
         const filePath = `/${key}/${index}-${fileName}`;
         const file = myBucket.file(filePath);
@@ -32,7 +33,9 @@ const images = require('./images.json');
     return promises.concat(arr);
   }, []);
 
-  const files = await Promise.all(uploads);
+  const files = await Q.allSettled(uploads).then((results) => {
+    return results.filter(r => r.state === 'fulfilled').map(r => r.value)
+  });
   const filesCsv = files.map(({path, tag}) => `${path},${tag}`).join(`\n`);
   const csv = new Readable();
   csv.push(filesCsv);
